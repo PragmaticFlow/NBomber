cab3f9cc8df704fccf63cf20b4ea47179e9713f5
alkondrashov <alexey.kondrashov91@gmail.com>
2018-08-10 15:35:02 +0100

    Moving examples with Assertions into separate folder

--- examples/NBomber.Examples.CSharp/Program.cs
+++ examples/NBomber.Examples.CSharp/Program.cs
@@ -15,19 +15,7 @@ namespace NBomber.Examples.CSharp
             //var scenario = HttpScenario.BuildScenario();  
             var scenario = SimplePushScenario.BuildScenario();
 
-            var assertions = new [] {
-               Assertion.ForScenario(stats => stats.OkCount > 95),
-               Assertion.ForScenario(stats => stats.FailCount > 95),
-               Assertion.ForScenario(stats => stats.ExceptionCount > 95),
-               Assertion.ForScenario(stats => stats.OkCount >= 95),
-               Assertion.ForTestFlow("Flow name 23", stats => stats.FailCount < 10),
-               Assertion.ForTestFlow("test flow1", stats => stats.FailCount < 10),
-               Assertion.ForStep("Flow name 1", "step name 10", stats => stats.FailCount == 95),
-               Assertion.ForStep("Flow name 1", "step name 19", stats => Array.IndexOf(new [] { 80, 95}, stats.FailCount) > -1),
-               Assertion.ForStep("Flow name 2", "step name 29", stats => stats.OkCount > 80 && stats.OkCount > 95)
-            };
-
-            ScenarioRunner.RunWithAssertions(scenario, assertions);
+            ScenarioRunner.Run(scenario);
         }
     }
 }
--- examples/NBomber.Examples.FSharp/Program.fs
+++ examples/NBomber.Examples.FSharp/Program.fs
@@ -4,21 +4,7 @@ open NBomber.Examples.FSharp.Scenarios
 open NBomber
 
 [<EntryPoint>]
-let main argv =
-    let assertions = [|
-       Assert.forScenario(fun stats -> stats.OkCount > 95)
-       Assert.forScenario(fun stats -> false)
-       Assert.forScenario(fun stats -> stats.FailCount > 95)
-       Assert.forScenario(fun stats -> stats.OkCount > 0)
-       Assert.forScenario(fun stats -> stats.OkCount > 0)
-       Assert.forScenario(fun stats -> stats.OkCount > 95)
-       Assert.forTestFlow("GET flow1", fun stats -> stats.FailCount < 10)
-       Assert.forTestFlow("GET flow", fun stats -> false)
-       Assert.forStep("GET flow", "GET github.com/VIP-Logic/NBomber html", fun stats -> false)
-       Assert.forStep("GET flow", "GET github.com/VIP-Logic/NBomber html", fun stats -> Seq.exists (fun i -> i = stats.FailCount) [80;95])
-       Assert.forStep("GET flow", "GET github.com/VIP-Logic/NBomber html", fun stats -> stats.OkCount > 80 && stats.OkCount > 95)
-    |]
-    
-    HttpScenario.buildScenario() |> Scenario.runWithAssertions(assertions)
+let main argv =    
+    HttpScenario.buildScenario() |> Scenario.run
 
     0 // return an integer exit code
