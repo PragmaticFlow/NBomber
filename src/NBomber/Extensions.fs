20226bdd6fe302c8f379573c450b080cff96084f
Antya Dev <antyadev@gmail.com>
2018-10-31 11:18:01 +0200

    Dev (#84)
    * cosmetic changes
    
    * removed docs
    
    * added dev branch to build on appveyor.yml
    
    * issue #83 - Add docer-compose file to run examples

---
+++ src/NBomber/Extensions.fs
@@ -0,0 +1,22 @@
+﻿[<AutoOpen>]
+module internal Extensions
+
+type Result<'T,'TError> with    
+    static member isOk(result) = 
+        match result with
+        | Ok _    -> true
+        | Error _ -> false
+    
+    static member isError(result) = not(Result.isOk(result))
+    
+    static member getError(result) = 
+        match result with
+        | Ok _     -> failwith "result is not error"
+        | Error er -> er
+
+module String =
+
+    let replace (oldValue: string, newValue: string) (str: string) =
+        str.Replace(oldValue, newValue)
+        
+
