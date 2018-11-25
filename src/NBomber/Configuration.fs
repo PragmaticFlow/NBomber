97aab0cf14c3b5c7d4206b1d463604970a38ab8f
Antya Dev <antyadev@gmail.com>
2018-11-25 23:12:18 +0200

    added missed files

---
+++ src/NBomber/Configuration.fs
@@ -0,0 +1,37 @@
+﻿namespace NBomber.Configuration
+
+open System
+
+type ScenarioSetting = {
+    ScenarioName: string
+    Duration: TimeSpan
+    ConcurrentCopies: int
+}
+
+type GlobalSettings = {
+    ScenariosSettings: ScenarioSetting[]
+    TargetScenarios: string[]
+}
+
+type NBomberConfig = {
+    GlobalSettings: GlobalSettings option    
+}
+
+type NBomberConfigJson = {
+    GlobalSettings: GlobalSettings  
+}
+
+module internal NBomberConfig =
+    open Newtonsoft.Json
+
+    let parse (json): NBomberConfig option =
+        
+        let parseGlobalSettings (config) =
+            if isNull(config.GlobalSettings :> obj) then None
+            else Some(config.GlobalSettings)        
+
+        let config = JsonConvert.DeserializeObject<NBomberConfigJson>(json)
+        if isNull(config :> obj) then None
+        else
+            let globalSettings = parseGlobalSettings(config)
+            Some({ GlobalSettings = globalSettings })
---
+++ src/NBomber/DomainServices/NBomberRunner.fs
@@ -0,0 +1,96 @@
+﻿module internal NBomber.DomainServices.NBomberRunner
+
+open System
+open System.Threading.Tasks
+
+open Serilog
+
+open NBomber.Contracts
+open NBomber.Configuration
+open NBomber.Domain
+open NBomber.Domain.Statistics
+open NBomber.Infra
+open NBomber.Infra.Dependency
+open NBomber.DomainServices.Reporting
+open NBomber.DomainServices.ScenarioRunner
+
+let tryGetScenariosSettings (context: NBomberRunnerContext) = maybe {
+    let! config = context.NBomberConfig
+    let! globalSettings = config.GlobalSettings
+    return! Option.ofObj(globalSettings.ScenariosSettings)
+}
+
+let applyScenariosSettings (scenarios: Scenario[], settings: ScenarioSetting[]) = 
+    scenarios
+
+let warmUpScenarios (scnRunners: ScenarioRunner[]) =
+    let results = 
+        scnRunners 
+        |> Array.map(fun x -> Log.Information("warming up scenario: '{0}'", x.Scenario.ScenarioName)
+                              x.RunInit())
+                                        
+    let allOk = results |> Array.forall(Result.isOk)
+    if not allOk then
+        results |> Errors.getErrorsString |> Log.Error
+        false
+    else
+        true
+
+let buildScenarios (context: NBomberRunnerContext) =    
+    match tryGetScenariosSettings(context) with
+    | Some settings -> applyScenariosSettings(context.Scenarios, settings)
+                       |> Array.map(Scenario.create)
+    
+    | None          -> context.Scenarios
+                       |> Array.map(Scenario.create)
+
+let displayProgress (scnRunners: ScenarioRunner[]) =
+    let longestDuration = scnRunners
+                          |> Array.map(fun x -> x.Scenario.Duration)
+                          |> Array.sort
+                          |> Array.tryHead
+                              
+    match longestDuration with
+    | Some v -> Dependency.ProgressBar.show(v)
+    | None   -> Task.FromResult()
+
+let waitUnitilAllFinish (scnRunners: ScenarioRunner[]) = 
+    let mutable allFinish = scnRunners |> Array.forall(fun x -> x.Finished)
+    while not allFinish do        
+        Task.Delay(TimeSpan.FromSeconds(1.0)).Wait()
+        allFinish <- scnRunners |> Array.forall(fun x -> x.Finished)
+
+let calcStatistics (scnRunners: ScenarioRunner[]) =
+    scnRunners
+    |> Array.map(fun x -> x.GetResult())
+    |> GlobalStats.create    
+
+let run (dep: Dependency, context: NBomberRunnerContext) =
+    Dependency.Logger.initLogger(dep.ApplicationType)
+    Log.Information("NBomber started a new session: '{0}'", dep.SessionId)
+
+    match Validation.validateRunnerContext(context) with
+    | Ok context ->             
+        let scnRunners = buildScenarios(context) |> Array.map(ScenarioRunner)                
+        
+        let allOk = warmUpScenarios(scnRunners)
+        
+        if allOk then
+            Log.Information("starting bombing...")
+            scnRunners |> Array.iter(fun x -> x.Run())
+            
+            if dep.ApplicationType = ApplicationType.Console then
+                displayProgress(scnRunners).Wait()
+
+            waitUnitilAllFinish(scnRunners)            
+
+            let globalStats = calcStatistics(scnRunners)
+            let allAsserts = scnRunners |> Array.collect(fun x -> x.Scenario.Assertions)
+            let assertResults = Assertion.apply(globalStats, allAsserts)
+            Report.build(dep, globalStats, assertResults)
+            |> Report.save(dep, "./")
+
+            if dep.ApplicationType = ApplicationType.Test then
+                TestFrameworkRunner.showResults(assertResults)        
+    
+    | Error e -> Log.Error(e)
