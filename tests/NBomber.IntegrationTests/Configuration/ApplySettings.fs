4c6cb76a9d36cd33605e25d0586089e8ac6734f7
Alex Kondrashov <Alexey.Kondrashov1@uk.ey.com>
2018-11-26 17:44:55 +0000

    adding first property based test

---
+++ tests/NBomber.IntegrationTests/Configuration/ApplySettings.fs
@@ -0,0 +1,14 @@
+﻿module Tests.Configuration.ApplySettings
+
+open NBomber.Contracts
+open NBomber.FSharp
+
+open FsCheck
+open FsCheck.Xunit
+
+[<Property>]
+let ``Apply scenario settings ``(xs:int) =
+    let scenario = Scenario.create("simple test", [])    
+    true
+
+    
