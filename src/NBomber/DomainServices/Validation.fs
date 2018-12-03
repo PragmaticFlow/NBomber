def023e6f138c6b6ceb4a990307b699e97b4f4ee
Alexey K <alexey.kondrashov91@gmail.com>
2018-12-03 06:13:09 +0000

    Adding basic rules to Validation.fs; adding couple tests for Validating context

--- src/NBomber/DomainServices/Validation.fs
+++ src/NBomber/DomainServices/Validation.fs
@@ -1,8 +1,36 @@
 ﻿module internal NBomber.DomainServices.Validation
 
 open NBomber.Contracts
+open NBomber.Configuration
+
+let configNotPreset (context : NBomberRunnerContext) =
+    match context.NBomberConfig with 
+    | Some config -> Ok(config)
+    | None -> Error "Config file is not present"
+
+let globalSettingsNotPresent (config : NBomberConfig) =
+    match config.GlobalSettings with 
+    | Some globalSettings -> Ok(globalSettings)
+    | None -> Error "Global settings is not present"
+    
+let targetScenarioIsNotPresent (globalSettings : GlobalSettings) =
+    let nonDeclaredScenarios = globalSettings.ScenariosSettings
+                                |> Array.map(fun x -> x.ScenarioName)
+                                |> Array.except <| globalSettings.TargetScenarios
+
+    match nonDeclaredScenarios with
+    | [||] -> Ok(globalSettings)
+    | _ -> Error "Target scenario is not declared"
 
 let validateRunnerContext(context: NBomberRunnerContext) = 
     // check on same scenario name
     // check on same step name within scenario
-    Ok(context)
+
+    let bind = Result.bind
+    let validationResult = configNotPreset
+                                >> bind globalSettingsNotPresent
+                                >> bind targetScenarioIsNotPresent <| context
+    
+    match validationResult with
+    | Ok _ -> Ok(context)
+    | Error msg -> Error(msg)
---
+++ tests/NBomber.IntegrationTests/Configuration/ValidateContext.fs
@@ -0,0 +1,36 @@
+﻿module Tests.Configuration.ValidateSettings
+
+open System
+
+open Xunit
+open FsCheck.Xunit
+
+open NBomber.FSharp
+open NBomber.Configuration
+open NBomber.DomainServices
+open NBomber.Contracts
+
+[<Property>]
+let ``Basic validation of scenario Config`` (name: string, duration: TimeSpan, concurrentCopies: int) =
+    let scenario = Scenario.create(name, [])
+    let settings = { ScenarioName = name; Duration = duration; ConcurrentCopies = concurrentCopies }
+    let globalSettings = { ScenariosSettings = [|settings|]; TargetScenarios = [|name|] }
+    let config = { NBomberConfig.GlobalSettings = Some globalSettings }
+    let context = { Scenarios = [|scenario|]; NBomberConfig = Some config }
+
+    match Validation.validateRunnerContext(context) with
+    | Ok context -> true
+    | _ -> false
+
+[<Fact>]
+let ``Target scenrio name is not declared`` () =
+    let scenario = Scenario.create("scenario name", [])
+    let globalSettings = { ScenariosSettings = [||]; TargetScenarios = [|"different scenario name"|] }
+    let config = { NBomberConfig.GlobalSettings = Some globalSettings }
+    let context = { Scenarios = [|scenario|]; NBomberConfig = Some config }
+
+    match Validation.validateRunnerContext(context) with
+    | Error "Target scenario is not declared" -> true
+    | _ -> false
+    |> Assert.True
+    
