3f6f8958715fdca615fa8cb9519f8d20dbc02511
Antya Dev <antyadev@gmail.com>
2018-12-04 13:12:54 +0200

    init

--- tests/NBomber.IntegrationTests/Step/Push.fs
+++ tests/NBomber.IntegrationTests/Step/Push.fs
@@ -7,42 +7,42 @@ open Xunit
 open NBomber.Contracts
 open NBomber.FSharp
 
-[<Fact>]
-let ``Ok and Fail should be properly count`` () =
+//[<Fact>]
+//let ``Ok and Fail should be properly count`` () =
     
-    let udaptesChannel = GlobalUpdatesChannel.Instance
+//    let udaptesChannel = GlobalUpdatesChannel.Instance
 
-    let correlationId = "push_test_0"
-    let push1 = Step.createPush("ok")
-    let push2 = Step.createPush("fail")
+//    let correlationId = "push_test_0"
+//    let push1 = Step.createPush("ok")
+//    let push2 = Step.createPush("fail")
 
-    let updatesTask = async {
-        do! Async.Sleep(100)
-        udaptesChannel.ReceivedUpdate("warm_up_step", push1.Name, Response.Ok())
-        do! Async.Sleep(100)
-        udaptesChannel.ReceivedUpdate("warm_up_step", push2.Name, Response.Ok())
+//    let updatesTask = async {
+//        do! Async.Sleep(100)
+//        udaptesChannel.ReceivedUpdate("warm_up_step", push1.Name, Response.Ok())
+//        do! Async.Sleep(100)
+//        udaptesChannel.ReceivedUpdate("warm_up_step", push2.Name, Response.Ok())
 
-        for i = 0 to 10 do            
-            do! Async.Sleep(100)
-            udaptesChannel.ReceivedUpdate(correlationId, push1.Name, Response.Ok())
+//        for i = 0 to 10 do            
+//            do! Async.Sleep(100)
+//            udaptesChannel.ReceivedUpdate(correlationId, push1.Name, Response.Ok())
             
-            do! Async.Sleep(100)
-            udaptesChannel.ReceivedUpdate(correlationId, push2.Name, Response.Fail(""))
-        return ()
-    }
+//            do! Async.Sleep(100)
+//            udaptesChannel.ReceivedUpdate(correlationId, push2.Name, Response.Fail(""))
+//        return ()
+//    }
 
-    let assertions = [
-       Assertion.forStep("ok", fun stats -> stats.OkCount >= 3 && stats.OkCount < 6)
-       Assertion.forStep("fail", fun stats -> stats.FailCount >= 3 && stats.FailCount < 6)
-    ]
+//    let assertions = [
+//       Assertion.forStep("ok", fun stats -> stats.OkCount >= 3 && stats.OkCount < 6)
+//       Assertion.forStep("fail", fun stats -> stats.FailCount >= 3 && stats.FailCount < 6)
+//    ]
 
-    let scenario =
-        Scenario.create("push_test", [push1; push2])
-        |> Scenario.withConcurrentCopies(1)
-        |> Scenario.withAssertions(assertions)
-        |> Scenario.withDuration(TimeSpan.FromSeconds(1.0))
+//    let scenario =
+//        Scenario.create("push_test", [push1; push2])
+//        |> Scenario.withConcurrentCopies(1)
+//        |> Scenario.withAssertions(assertions)
+//        |> Scenario.withDuration(TimeSpan.FromSeconds(1.0))
     
-    updatesTask |> Async.Start
+//    updatesTask |> Async.Start
 
-    NBomberRunner.registerScenarios [scenario]
-    |> NBomberRunner.runTest
+//    NBomberRunner.registerScenarios [scenario]
+//    |> NBomberRunner.runTest
