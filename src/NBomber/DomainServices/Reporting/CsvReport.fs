f867dce53085911f4ddcea64b7161edc3809c325
Alex Kondrashov <Alexey.Kondrashov1@uk.ey.com>
2018-12-18 19:27:02 +0000

    adding csv report class

---
+++ src/NBomber/DomainServices/Reporting/CsvReport.fs
@@ -0,0 +1,26 @@
+﻿module internal rec NBomber.DomainServices.Reporting.CsvReport
+
+open System
+open ConsoleTables
+open NBomber.Domain.StatisticsTypes
+
+let print (stats: GlobalStats) =
+    let header = printHeader()
+
+    stats.AllScenariosStats
+    |> Array.map(fun x -> printSteps(x))
+    |> String.concat(Environment.NewLine)
+
+let private printHeader () =
+    "Scenario, Execution time, request_count, OK, failed, RPS, min, mean, max, 50%, 75%, 95%, StdDev"
+
+let private printSteps (stats: ScenarioStats) : string [] =
+    stats.StepsStats
+    |> Array.map(fun s ->
+        String.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}",
+            stats.ScenarioName, stats.Duration.ToString(),
+            s.StepName, s.ReqeustCount, s.OkCount, s.FailCount,
+            s.Percentiles.Value.RPS, s.Percentiles.Value.Min,
+            s.Percentiles.Value.Mean, s.Percentiles.Value.Max,
+            s.Percentiles.Value.Percent50, s.Percentiles.Value.Percent75,
+            s.Percentiles.Value.Percent95, s.Percentiles.Value.StdDev))
--- src/NBomber/DomainServices/Reporting/Report.fs
+++ src/NBomber/DomainServices/Reporting/Report.fs
@@ -13,13 +13,15 @@ open NBomber.Infra.Dependency
 
 type ReportResult = {
     TxtReport: string
-    HtmlReport: string    
+    HtmlReport: string
+    CsvReport: string
 }
 
 let build (dep: Dependency, stats: GlobalStats,
            assertResults: Result<Assertion,DomainError>[]) =
     { TxtReport = TxtReport.print(stats)
-      HtmlReport = HtmlReport.print(dep, stats) }
+      HtmlReport = HtmlReport.print(dep, stats)
+      CsvReport = CsvReport.print(stats) }
 
 let save (dep: Dependency, outPutDir: string) (report: ReportResult) =
     try
@@ -30,7 +32,8 @@ let save (dep: Dependency, outPutDir: string) (report: ReportResult) =
         let filePath = reportsDir + "/report_" + dep.SessionId
 
         File.WriteAllText(filePath + ".txt", report.TxtReport)
-        File.WriteAllText(filePath + ".html", report.HtmlReport)    
+        File.WriteAllText(filePath + ".html", report.HtmlReport)
+        File.WriteAllText(filePath + ".csv", report.CsvReport)    
         Log.Information("reports saved in folder: '{0}', {1}", DirectoryInfo(reportsDir).FullName, Environment.NewLine)
         Log.Information(report.TxtReport)
     with
