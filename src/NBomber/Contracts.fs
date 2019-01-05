9b36412bc75ffbd2faca409e541ef405b7acba29
Alex K <alexey.kondrashov91@gmail.com>
2019-01-05 21:26:14 +0000

    adding overload constructor for Result Ok

--- src/NBomber/Contracts.fs
+++ src/NBomber/Contracts.fs
@@ -60,6 +60,7 @@ type AssertStats = {
     
 type Response with
     static member Ok([<Optional;DefaultParameterValue(null:obj)>]payload: obj, [<Optional;DefaultParameterValue(0:int)>]sizeBytes: int) = { IsOk = true; Payload = payload; SizeBytes = sizeBytes }
+    static member Ok(payload: byte[]) = { IsOk = true; Payload = payload; SizeBytes = if payload <> null then Array.length payload else 0}
     static member Fail() = { IsOk = false; Payload = null; SizeBytes = 0 }
 
 type ReportFormat = 
---
+++ tests/NBomber.IntegrationTests/Result.fs
@@ -0,0 +1,12 @@
+﻿module Tests.Result
+
+open NBomber.Contracts
+
+open Xunit
+open FsCheck.Xunit
+
+[<Property>]
+let ``SizeBytes in Response object should equal size of incoming payload`` (payload: byte[]) =
+    let actual = Response.Ok(payload)
+    if payload = null then Assert.Equal(0, actual.SizeBytes)
+    else Assert.Equal(Array.length payload, actual.SizeBytes)
