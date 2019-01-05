a82b9bdeec592fb2147fa7225f698ca89e88fe99
Alex K <alexey.kondrashov91@gmail.com>
2019-01-05 22:10:34 +0000

    renaming test

--- tests/NBomber.IntegrationTests/Response.fs
+++ tests/NBomber.IntegrationTests/Response.fs
@@ -6,7 +6,7 @@ open Xunit
 open FsCheck.Xunit
 
 [<Property>]
-let ``Response.Ok() should calculate SizeBytes when passing array of bytes in`` (payload: byte[]) =
+let ``Response.Ok(payload: byte[]) should calculate SizeBytes automatically`` (payload: byte[]) =
     let actual = Response.Ok(payload)
     if isNull(payload) then Assert.Equal(0, actual.SizeBytes)
-    else Assert.Equal(Array.length payload, actual.SizeBytes)
+    else Assert.Equal(Array.length(payload), actual.SizeBytes)
