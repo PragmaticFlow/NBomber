171b455fcd9ee99212e3f376eff41bd1b3d6fef8
Antya Dev <antyadev@gmail.com>
2019-01-08 23:06:49 +0200

    added StepResults type to simplify working with percentailes (#113)

--- examples/FSharp/FSharp.Examples/Program.fs
+++ examples/FSharp/FSharp.Examples/Program.fs
@@ -8,7 +8,7 @@ let main argv =
     HttpScenario.buildScenario()
     //HelloWorldScenario.buildScenario()
     |> Scenario.withConcurrentCopies(10)
-    |> Scenario.withWarmUpDuration(TimeSpan.FromSeconds(10.0))    
+    //|> Scenario.withWarmUpDuration(TimeSpan.FromSeconds(10.0))    
     |> Scenario.withDuration(TimeSpan.FromSeconds(5.0))        
     |> NBomberRunner.registerScenario
     // |> NBomberRunner.registerScenarios
