2a180dde3025b91470c7113674fedd071c9a2281
Antya Dev <antyadev@gmail.com>
2019-02-13 08:19:29 +0200

    fixed bug with applying assertions

--- examples/CSharp/CSharp.Examples.NUnit/Tests.cs
+++ examples/CSharp/CSharp.Examples.NUnit/Tests.cs
@@ -14,8 +14,8 @@ namespace CSharp.Examples.NUnit
         {   
             var step1 = Step.CreateAction("simple step", ConnectionPool.None, async context =>
             {
-                await Task.Delay(TimeSpan.FromSeconds(0.5));
-                return Response.Ok();
+                await Task.Delay(TimeSpan.FromSeconds(0.1));
+                return Response.Ok(sizeBytes: 1024);
             });
 
             return ScenarioBuilder.CreateScenario("nunit hello world", step1);                
@@ -25,7 +25,11 @@ namespace CSharp.Examples.NUnit
         public void Test()
         {
             var assertions = new[] {               
-               Assertion.ForStep("simple step", stats => stats.OkCount > 2, "OkCount > 2")
+               Assertion.ForStep("simple step", stats => stats.OkCount > 2, "OkCount > 2"),
+               Assertion.ForStep("simple step", stats => stats.RPS > 8, "RPS > 8"),
+               Assertion.ForStep("simple step", stats => stats.Percent75 >= 102, "Percent75 >= 1000"),
+               Assertion.ForStep("simple step", stats => stats.DataMinKb == 1.0, "DataMinKb == 1.0"),
+               Assertion.ForStep("simple step", stats => stats.AllDataMB >= 0.01, "AllDataMB >= 0.01")
             };
 
             var scenario = BuildScenario()
--- examples/FSharp/FSharp.Examples.XUnit/Tests.fs
+++ examples/FSharp/FSharp.Examples.XUnit/Tests.fs
@@ -11,9 +11,9 @@ open NBomber.FSharp
 
 let buildScenario () =
 
-    let step1 = Step.createAction("Step", ConnectionPool.none, fun context -> task {
-        do! Task.Delay(TimeSpan.FromSeconds(0.5))
-        return Response.Ok()
+    let step1 = Step.createAction("simple step", ConnectionPool.none, fun context -> task {
+        do! Task.Delay(TimeSpan.FromSeconds(0.1))
+        return Response.Ok(sizeBytes = 1024)
     })
 
     Scenario.create("xunit hello world", [step1])    
@@ -22,12 +22,16 @@ let buildScenario () =
 let ``XUnit test`` () =    
     
     let assertions = [       
-       Assertion.forStep("Step", (fun stats -> stats.OkCount > 2), "OkCount > 2")
+       Assertion.forStep("simple step", (fun stats -> stats.OkCount > 2), "OkCount > 2")
+       Assertion.forStep("simple step", (fun stats -> stats.RPS > 8), "RPS > 8")
+       Assertion.forStep("simple step", (fun stats -> stats.Percent75 >= 102), "Percent75 >= 102")
+       Assertion.forStep("simple step", (fun stats -> stats.DataMinKb = 1.0), "DataMinKb = 1.0")
+       Assertion.forStep("simple step", (fun stats -> stats.AllDataMB >= 0.01), "AllDataMB >= 0.01")
     ]
 
     buildScenario()
     |> Scenario.withConcurrentCopies(1)
-    |> Scenario.withWarmUpDuration(TimeSpan.FromSeconds(1.0))
+    |> Scenario.withWarmUpDuration(TimeSpan.FromSeconds(0.0))
     |> Scenario.withDuration(TimeSpan.FromSeconds(2.0))
     |> Scenario.withAssertions(assertions)
     |> NBomberRunner.registerScenario
