fcc0aca5358091751305c7846a2020ac103a81a6
Antya Dev <antyadev@gmail.com>
2019-05-15 00:07:26 +0300

    cosmetic changes for Http example

--- examples/CSharp/CSharp.Examples/Scenarios/Http.cs
+++ examples/CSharp/CSharp.Examples/Scenarios/Http.cs
@@ -7,33 +7,22 @@ using NBomber.CSharp;
 namespace CSharp.Examples.Scenarios
 {
     class HttpScenario
-    {
+    {   
         public static Scenario BuildScenario()
         {
-            HttpRequestMessage CreateHttpRequest(Uri requestUrl)
-            {
-                var msg = new HttpRequestMessage();
-                msg.RequestUri = requestUrl;
-                msg.Headers.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-                msg.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
-                return msg;
-            }
+            // it's a very basic HTTP example, don't use it for production testing
+            // for production purposes use NBomber.Http which use performance optimizations
+            // you can find more here: https://github.com/PragmaticFlow/NBomber.Http
 
             var httpClient = new HttpClient();
             var url = new Uri("https://gitter.im");
 
             var step1 = Step.Create("GET html", ConnectionPool.None, async context =>
-            {
-                var request = CreateHttpRequest(url);
-
-                var response = await httpClient.SendAsync(request, context.CancellationToken);
-
-                var responseSize = response.Content.Headers.ContentLength.HasValue
-                    ? (int) response.Content.Headers.ContentLength.Value
-                    : 0;
+            {   
+                var response = await httpClient.GetAsync("https://gitter.im");
 
                 return response.IsSuccessStatusCode
-                    ? Response.Ok(sizeBytes: responseSize)
+                    ? Response.Ok()
                     : Response.Fail();
             });
 
