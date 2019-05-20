3a1abcca549b5004f14f59f7d1614ed63d3b8492
Danylo Bondarenko <danylo.b@sbtech.com>
2019-05-20 13:15:19 +0300

    removed redundant example

--- examples/CSharp/CSharp.Examples/Scenarios/HelloWorld.cs
+++ examples/CSharp/CSharp.Examples/Scenarios/HelloWorld.cs
@@ -9,36 +9,21 @@ namespace CSharp.Examples.Scenarios
     {
         public static Scenario BuildScenario()
         {   
-            var step1 = Step.Create("simple step", ConnectionPool.None, async context =>
+            var step1 = Step.Create("simple step #1", ConnectionPool.None, async context =>
             {
                 // you can do any logic here: go to http, websocket etc
                 await Task.Delay(TimeSpan.FromSeconds(0.1), context.CancellationToken);
                 return Response.Ok();
             });
-            return ScenarioBuilder.CreateScenario("Hello World from NBomber!", step1);
-        }
 
-        public static Scenario BuildPlainScenario()
-        {
 
-            //Just a sample how to create no-context step
-            var step1 = Step.Create("plain step", () => Task.FromResult(Response.Ok()));
-            return ScenarioBuilder.CreateScenario("Hello World from NBomber struggle continues!", step1);
-        }
-    }
-
-    class HelloWorld2Scenario
-    {
-        public static Scenario BuildScenario()
-        {
-
-            //Just a sample how to create no-context step
-            var step1 = Step.Create("plain step", async () =>
+            var step2 = Step.Create("plain step", async  =>
             {
-                await Task.Delay(TimeSpan.FromSeconds(0.1));
-                return Response.Ok();
+                await Task.Delay(TimeSpan.FromSeconds(0.1), context.CancellationToken);
+                return Task.FromResult(Response.Ok());
             });
-            return ScenarioBuilder.CreateScenario("Hello World from NBomber struggle continues!", step1);
+           
+            return ScenarioBuilder.CreateScenario("Hello World from NBomber!", step1, step2);
         }
     }
 }
