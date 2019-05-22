c2b9ea183fbc2eaf2b472e28ded92ffdb7def937
Andreas Vilinski <vilinski@yahoo.de>
2019-05-22 11:57:36 +0200

    support expecto test framework (#150)

---
+++ examples/FSharp/FSharp.Examples.Expecto/Main.fs
@@ -0,0 +1,6 @@
+﻿module FSharp.Examples.Expecto
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
--- src/NBomber/Infra/TestFrameworkRunner.fs
+++ src/NBomber/Infra/TestFrameworkRunner.fs
@@ -6,6 +6,7 @@ open NBomber.Errors
 type TestFramework =
     | Xunit of Type
     | Nunit of Type
+    | Expecto of Type
 
 let private printErrorMessage (framework: TestFramework) (errorsMessage: string) =
     match framework with
@@ -19,16 +20,21 @@ let private printErrorMessage (framework: TestFramework) (errorsMessage: string)
         let func = Delegate.CreateDelegate(typeof<Action<string>>, m) :?> (Action<string>)
         func.Invoke(errorsMessage)
 
+    | Expecto assertException ->
+        let ctor = assertException.GetConstructor [|typeof<string>|]
+        ctor.Invoke [| errorsMessage|] :?> exn |> raise
+
 let tryGetCurrentFramework () =
     ["Xunit.Assert, xunit", Xunit
      "Xunit.Assert, xunit.assert", Xunit
-     "NUnit.Framework.Assert, nunit.framework", Nunit]
-     |> List.tryPick(fun (typeName, framework) ->
+     "NUnit.Framework.Assert, nunit.framework", Nunit
+     "Expecto.AssertException, expecto", Expecto
+    ] |> List.tryPick(fun (typeName, framework) ->
         typeName |> Type.GetType |> Option.ofObj |> Option.map(framework))    
 
 let showErrors (error: AppError[]) =
     match tryGetCurrentFramework() with
-    | None -> failwith "Unknown framework"
+    | None -> failwith "No supported test framework found. Supported are Xunit, NUnit, Expecto"
     | Some framework -> 
         error
         |> Array.map(AppError.toString)
