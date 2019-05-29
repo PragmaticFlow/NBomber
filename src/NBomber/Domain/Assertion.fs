796c1238c5af0f9630188c5d92049250cd9a4d96
Antya Dev <antyadev@gmail.com>
2019-05-29 07:27:01 +0300

    added missed change

--- src/NBomber/Domain/Assertion.fs
+++ src/NBomber/Domain/Assertion.fs
@@ -4,8 +4,8 @@ module internal NBomber.Domain.Assertion
 open NBomber.Contracts
 open NBomber.Errors
 
-let create (assertions: IAssertion[]) = 
-    assertions |> Array.map(fun x -> x :?> Assertion)
+let create (assertions: IAssertion seq) = 
+    assertions |> Seq.map(fun x -> x :?> Assertion) |> Seq.toArray
 
 let apply (stepsStats: Statistics[]) (assertions: Assertion[]) =    
     assertions
