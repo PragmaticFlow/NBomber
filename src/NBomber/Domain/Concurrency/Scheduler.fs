0cb0605ddf380949b088a279a2b5286bcbb6261a
antyadev <antyadev@gmail.com>
2020-01-26 15:12:31 +0200

    added some layout for ActorsPool

--- src/NBomber/Domain/Concurrency/Scheduler.fs
+++ src/NBomber/Domain/Concurrency/Scheduler.fs
@@ -8,9 +8,9 @@ open NBomber.Contracts
 open NBomber.Errors
 
 type SchedulerResult =
-    | AddConstantCopies    of count:uint32
-    | RemoveConstantCopies of count:uint32
-    | AddOneTimeCopies     of count:uint32
+    | AddConstantActors    of count:uint32
+    | RemoveConstantActors of count:uint32
+    | AddOneTimeActors     of count:uint32
     | DoNothing
 
 let schedule (invokeSchedulePerSec: uint32,
@@ -19,23 +19,23 @@ let schedule (invokeSchedulePerSec: uint32,
 
     match runningStrategy with
     | Constant (copiesCount, duration) ->
-        if constantRunCopiesCount < copiesCount then [AddConstantCopies(copiesCount - constantRunCopiesCount)]
-        elif constantRunCopiesCount > copiesCount then [RemoveConstantCopies(constantRunCopiesCount - copiesCount)]
+        if constantRunCopiesCount < copiesCount then [AddConstantActors(copiesCount - constantRunCopiesCount)]
+        elif constantRunCopiesCount > copiesCount then [RemoveConstantActors(constantRunCopiesCount - copiesCount)]
         else [DoNothing]
 
     | AddPerSec (copiesCount, duration) ->
-        [RemoveConstantCopies(constantRunCopiesCount)
-         AddOneTimeCopies(copiesCount / invokeSchedulePerSec)]
+        [RemoveConstantActors(constantRunCopiesCount)
+         AddOneTimeActors(copiesCount / invokeSchedulePerSec)]
 
     | RampTo (copiesCount, duration) ->
         if constantRunCopiesCount < copiesCount then
             let copiesToStart = copiesCount - constantRunCopiesCount
             let copiesPerSec = copiesToStart / uint32 duration.TotalSeconds
-            [AddConstantCopies(copiesPerSec / invokeSchedulePerSec)]
+            [AddConstantActors(copiesPerSec / invokeSchedulePerSec)]
 
         elif constantRunCopiesCount > copiesCount then
             let copiesToStop = constantRunCopiesCount - copiesCount
             let copiesPerSec = copiesToStop / uint32 duration.TotalSeconds
-            [RemoveConstantCopies(copiesPerSec / invokeSchedulePerSec)]
+            [RemoveConstantActors(copiesPerSec / invokeSchedulePerSec)]
 
         else [DoNothing]
