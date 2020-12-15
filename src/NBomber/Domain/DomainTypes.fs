346ce9099a66b93cd4531d1f27e913958b13430e
Antya Dev <antyadev@gmail.com>
2020-12-15 19:48:47 +0200

    refactored step context with small perf improvements (#278)

--- src/NBomber/Domain/DomainTypes.fs
+++ src/NBomber/Domain/DomainTypes.fs
@@ -1,11 +1,14 @@
 module internal NBomber.Domain.DomainTypes
 
 open System
+open System.Threading
 open System.Threading.Tasks
 
-open NBomber
+open Serilog
+
 open NBomber.Contracts
 open NBomber.Domain.ConnectionPool
+open NBomber.Extensions.InternalExtensions
 
 //todo: use opaque types
 type StepName = string
@@ -16,38 +19,23 @@ type StopCommand =
     | StopScenario of ScenarioName * reason:string
     | StopTest of reason:string
 
-type StepContext<'TConnection,'TFeedItem>(correlationId, cancellationToken,
-                                          connection, data, feedItem, logger,
-                                          invocationCount, execStopCommand: StopCommand -> unit) =
-    interface IStepContext<'TConnection,'TFeedItem> with
-        member _.CorrelationId = correlationId
-        member _.CancellationToken = cancellationToken
-        member _.Connection = connection
-        member _.Data = data
-        member _.FeedItem = feedItem
-        member _.Logger = logger
-        member _.InvocationCount = invocationCount
-
-        member _.GetPreviousStepResponse<'T>() =
-            try
-                let prevStepResponse = data.[Constants.StepResponseKey]
-                if isNull prevStepResponse then
-                    Unchecked.defaultof<'T>
-                else
-                    prevStepResponse :?> 'T
-            with
-            | ex -> Unchecked.defaultof<'T>
-
-        member _.StopScenario(scenarioName, reason) = StopScenario(scenarioName, reason) |> execStopCommand
-        member _.StopCurrentTest(reason) = StopTest(reason) |> execStopCommand
-
-    member _.ExecStopCommand(command) = execStopCommand(command)
+type UntypedStepContext = {
+    CorrelationId: CorrelationId
+    CancellationToken: CancellationToken
+    Connection: obj
+    Logger: ILogger
+    mutable FeedItem: obj
+    mutable Data: Dict<string,obj>
+    mutable InvocationCount: int
+    StopScenario: string * string -> unit // scenarioName * reason
+    StopCurrentTest: string -> unit       // reason
+}
 
 type Step = {
     StepName: StepName
     ConnectionPoolArgs: ConnectionPoolArgs<obj> option
     ConnectionPool: ConnectionPool option
-    Execute: StepContext<obj,obj> -> Task<Response>
+    Execute: UntypedStepContext -> Task<Response>
     Feed: IFeed<obj>
     DoNotTrack: bool
 } with
@@ -57,8 +45,7 @@ type Step = {
 
 type RunningStep = {
     Value: Step
-    mutable Context: StepContext<obj,obj>
-    mutable InvocationCount: int
+    Context: UntypedStepContext
 }
 
 type StepResponse = {
