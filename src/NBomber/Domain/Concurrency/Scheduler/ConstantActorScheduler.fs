04ca1f5eb8b639fb0c6e078e9ea5ac1de7d80025
Antya Dev <antyadev@gmail.com>
2021-01-07 18:53:52 +0200

    simplified actor schedulers and improved performance. (#302)

--- src/NBomber/Domain/Concurrency/Scheduler/ConstantActorScheduler.fs
+++ src/NBomber/Domain/Concurrency/Scheduler/ConstantActorScheduler.fs
@@ -1,94 +1,67 @@
 module internal NBomber.Domain.Concurrency.Scheduler.ConstantActorScheduler
 
-open System.Collections.Generic
-open System.Threading.Tasks
+open System
 
-open FSharp.Control.Tasks.NonAffine
-
-open NBomber.Extensions.InternalExtensions
 open NBomber.Domain.Concurrency
 open NBomber.Domain.Concurrency.ScenarioActor
-
-type ActorTask = Task
 
 [<Struct>]
 type SchedulerCommand =
     | KeepWorking
-    | ReleaseFinishedActor
-    | AddActors of count:int
+    | AddActors of addCount:int
+    | RemoveActor of removeCount:int
     | StopScheduler
 
-let removeFromScheduler (scheduledActorsCount, removeCount) =
+let removeFromScheduler scheduledActorsCount removeCount =
     let actorsCount = scheduledActorsCount - removeCount
     if actorsCount < 0 then 0
     else actorsCount
 
-let schedule (workingActorCount, scheduledActorCount) =
+let schedule workingActorCount scheduledActorCount =
     if scheduledActorCount = 0 then StopScheduler
     elif workingActorCount = scheduledActorCount then KeepWorking
     elif workingActorCount < scheduledActorCount then AddActors(scheduledActorCount - workingActorCount)
-    else ReleaseFinishedActor
+    else RemoveActor(workingActorCount - scheduledActorCount)
 
 type ConstantActorScheduler(dep: ActorDep) =
 
     let mutable _actorPool = List.empty<ScenarioActor>
-    let _workingActors = Dictionary<ActorTask,ScenarioActor>()
     let mutable _scheduledActorCount = 0
-    let mutable _stop = true
+    let createActors = ScenarioActorPool.createActors dep
 
     let stop () =
-        _stop <- true
-        ScenarioActorPool.releaseActors(_actorPool)
+        ScenarioActorPool.stopActors _actorPool
+        _scheduledActorCount <- 0
 
-    let startScheduler () =
+    let execScheduler (scheduledActorCount) =
+        let workingActors = ScenarioActorPool.getWorkingActors _actorPool
+        match schedule workingActors.Length scheduledActorCount with
+        | KeepWorking -> ()
+        | AddActors count ->
+            let result = ScenarioActorPool.rentActors createActors _actorPool count
+            _actorPool <- ScenarioActorPool.updatePool _actorPool result
+            result.ActorsFromPool |> List.iter(fun x -> x.RunInfinite() |> ignore)
+            result.NewActors|> List.iter(fun x -> x.RunInfinite() |> ignore)
 
-        _stop <- false
+        | RemoveActor count ->
+            workingActors
+            |> List.take count
+            |> List.iter(fun x -> x.Stop())
 
-        let waitActors (workingActors: Dict<ActorTask,ScenarioActor>) = task {
-            let! finishedTask = Task.WhenAny(workingActors.Keys)
-            let finishedActor = workingActors.[finishedTask]
-            workingActors.Remove(finishedTask) |> ignore
-            return finishedActor
-        }
+        | StopScheduler -> stop()
 
-        let addAndExecActor (actor: ScenarioActor) =
-            let startedActorTask = actor.ExecSteps()
-            _workingActors.[startedActorTask] <- actor
-
-        task {
-            do! Task.Yield()
-
-            while not _stop || not dep.CancellationToken.IsCancellationRequested do
-
-                try
-                    match schedule(_workingActors.Count, _scheduledActorCount) with
-                    | KeepWorking          ->
-                        let! finishedActor = waitActors(_workingActors)
-                        addAndExecActor(finishedActor)
-
-                    | ReleaseFinishedActor ->
-                        let! finishedActor = waitActors(_workingActors)
-                        finishedActor.ReserveForScheduler()
-
-                    | AddActors count ->
-                        let result = ScenarioActorPool.rentActors(dep, _actorPool, count)
-                        _actorPool <- ScenarioActorPool.updatePool(_actorPool, result)
-                        result.ActorsFromPool @ result.NewActors |> List.iter(addAndExecActor)
-
-                    | StopScheduler -> stop()
-                with
-                | ex -> dep.Logger.Fatal(ex.ToString())
-
-            stop()
-        }
-
+    member _.ScheduledActorCount = _scheduledActorCount
     member _.AvailableActors = _actorPool
-    member _.WorkingActorCount = _scheduledActorCount
-    member _.Stop() = stop()
 
     member _.AddActors(count) =
         _scheduledActorCount <- _scheduledActorCount + count
-        if _stop then startScheduler() |> ignore
+        execScheduler _scheduledActorCount
 
     member _.RemoveActors(count) =
-        _scheduledActorCount <- removeFromScheduler(_scheduledActorCount, count)
+        _scheduledActorCount <- removeFromScheduler _scheduledActorCount count
+        execScheduler _scheduledActorCount
+
+    member _.Stop() = stop()
+
+    interface IDisposable with
+        member _.Dispose() = stop()
