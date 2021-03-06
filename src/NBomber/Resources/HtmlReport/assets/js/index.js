28de33aad0315677145aea3cec8e5a449e6d45ff
Yaroshvitaliy <yaroshenko.vitaliy@gmail.com>
2021-03-06 11:57:13 +0200

    Html report error stats (#327)
    * correct duration time for stats
    
    * added error stats in html report
    
    * added scenario stats component, removed footers from stats in html report
    
    Co-authored-by: antyadev <antyadev@gmail.com>

--- src/NBomber/Resources/HtmlReport/assets/js/index.js
+++ src/NBomber/Resources/HtmlReport/assets/js/index.js
@@ -159,7 +159,7 @@ const initApp = (appContainer, viewModel) => {
         },
         plotOptions: createPlotOptions(),
         legend: {
-            labelFormat: '{name}: {y}',
+            labelFormat: '{name}',
             align: 'right',
             verticalAlign:'middle',
             padding: 0
@@ -205,7 +205,7 @@ const initApp = (appContainer, viewModel) => {
         },
         plotOptions: createPlotOptions(),
         legend: {
-            labelFormat: '{name}: {y}',
+            labelFormat: '{name}',
             align: 'right',
             verticalAlign:'middle',
             padding: 0
@@ -529,24 +529,49 @@ const initApp = (appContainer, viewModel) => {
         template: '#node-info-table-template'
     });
 
-    Vue.component('scenario-stats-table', {
+    Vue.component('scenario-stats', {
         props: ['scenarioStats'],
-        template: '#scenario-stats-table-template'
+        template: '#scenario-stats-template'
+    });
+
+    Vue.component('scenario-stats-table', {
+        props: ['scenarioStats', 'failStats'],
+        template: '#scenario-stats-table-template',
+        data: function () {
+            return {
+                statsName: this.failStats ? "Fail" : "Ok"
+            }
+        }
     });
 
     Vue.component('scenario-stats-requests-number-table', {
-        props: ['scenarioStats'],
-        template: '#scenario-stats-requests-number-table-template'
+        props: ['scenarioStats', 'failStats'],
+        template: '#scenario-stats-requests-number-table-template',
+        data: function () {
+            return {
+                statsName: this.failStats ? "Fail" : "Ok"
+            }
+        }
     });
 
     Vue.component('scenario-stats-latency-table', {
-        props: ['scenarioStats'],
-        template: '#scenario-stats-latency-table-template'
+        props: ['scenarioStats', 'failStats'],
+        template: '#scenario-stats-latency-table-template',
+        data: function () {
+            return {
+                statsName: this.failStats ? "Fail" : "Ok"
+            }
+        }
     });
 
     Vue.component('scenario-stats-data-transfer-table', {
-        props: ['scenarioStats'],
-        template: '#scenario-stats-data-transfer-table-template'
+        props: ['scenarioStats', 'failStats'],
+        template: '#scenario-stats-data-transfer-table-template',
+        data: function () {
+            return {
+                statsName: this.failStats ? "Fail" : "Ok"
+            }
+        }
     });
 
     const convertPluginStats = pluginsStats =>
