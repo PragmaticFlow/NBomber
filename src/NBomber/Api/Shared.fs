49643e17f9f840ea5304ad40d5469378fbeff5b5
antyadev <antyadev@gmail.com>
2022-12-16 10:15:19 +0200

    added ClientPool<T>

--- src/NBomber/Api/Shared.fs
+++ src/NBomber/Api/Shared.fs
@@ -1,8 +1,42 @@
 namespace NBomber
 
 open System
+open System.Collections.Generic
 open System.Runtime.CompilerServices
+open NBomber.Contracts
 open NBomber.Extensions.Internal
+
+type ClientPool<'T>() as this =
+
+    let mutable _disposed = false
+    let _clients = ResizeArray<'T>()
+
+    member _.Clients = _clients :> IReadOnlyList<_>
+    member _.AddClient(client) = _clients.Add client
+
+    member _.GetClient(scenarioInfo: ScenarioInfo) =
+        let index = scenarioInfo.ThreadNumber % _clients.Count
+        _clients[index]
+
+    member _.DisposeClients() =
+        if not _disposed then
+            _disposed <- true
+
+            for c in _clients do
+                match box c with
+                | :? IDisposable as client -> client.Dispose()
+                | _ -> ()
+
+    member _.DisposeClients(disposeClient: Action<'T>) =
+        if not _disposed then
+            _disposed <- true
+
+            for c in _clients do
+                disposeClient.Invoke c
+
+    interface IDisposable with
+        member _.Dispose() =
+            this.DisposeClients()
 
 module Converter =
 
