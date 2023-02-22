aaff058a99c05ed64b02d9ba3ad75bfa573a7280
antyadev <antyadev@gmail.com>
2023-02-22 10:36:33 +0200

    init

---
+++ examples/CSharpProd/DB/ScyllaDB/ScyllaDBTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Net;
+using System.Text;
+using Cassandra;
+using Cassandra.Data.Linq;
+using Cassandra.Mapping;
+using NBomber.CSharp;
+using NBomber.Data;
+
+namespace CSharpProd.DB.ScyllaDB;
+
+public class User
+{
+    public string Id { get; set; }
+    public byte[] Data { get; set; }
+}
+
+public class ScyllaDBTests
+{
+    public void Run(string[] args)
+    {
+        var initDbScn = new ScyllaInitDBScenario();
+        var random = new Random();
+
+        var getById = Scenario.Create("get_by_id", async context =>
+        {
+            var randomId = random.Next(1, initDbScn.DBSettings.UserCount);
+            var query = initDbScn.GetByIdQuery.Bind(randomId.ToString());
+            var response = await initDbScn.Session.ExecuteAsync(query);
+            
+            // var response = await initDbScn.Session.ExecuteAsync(
+            //     new SimpleStatement($"SELECT id, data FROM myspace.users WHERE id='{randomId}'")
+            // );
+
+            return response.Columns.Length > 0
+                ? Response.Ok(initDbScn.DBSettings.UserRecordSizeBytes)
+                : Response.Fail(statusCode: "not found");
+        });
+        
+        var readModifyWrite = Scenario.Create("read_modify_write", async context =>
+        {
+            var randomId = random.Next(1, initDbScn.DBSettings.UserCount);
+            var readQuery = initDbScn.GetByIdQuery.Bind(randomId.ToString());
+            var response = await initDbScn.Session.ExecuteAsync(readQuery);
+
+            var writeQuery = initDbScn.InsertQuery.Bind(randomId.ToString(), initDbScn.UserRecord);
+            response = await initDbScn.Session.ExecuteAsync(writeQuery);
+
+            return response.IsFullyFetched
+                ? Response.Ok(initDbScn.DBSettings.UserRecordSizeBytes * 2)
+                : Response.Fail(statusCode: "not found");
+        });
+        
+        NBomberRunner
+            .RegisterScenarios(initDbScn.Create(), getById, readModifyWrite)
+            .LoadConfig("DB/ScyllaDB/config.json")
+            .Run(args);
+
+        // // var config = new MappingConfiguration().Define(
+        // //     new Map<User>()
+        // //         .KeyspaceName("myspace")
+        // //         .TableName("users")
+        // //         .PartitionKey(a => a.Id)
+        // //         //.CompactStorage()
+        // // );
+        //
+        // // var table = new Table<User>(session, config);
+        // //table.CreateIfNotExists();
+        //
+        // session.ChangeKeyspace("myspace");
+        // session.Execute(new SimpleStatement(
+        //     "CREATE TABLE users (id text PRIMARY KEY, data blob) WITH COMPACT STORAGE AND compression = {}")
+        // );
+        //
+        // var query = session.Prepare("INSERT INTO users (id, data) VALUES (?, ?)");
+        // var record = Data.GenerateRandomBytes(800);
+        //
+        // var batch = new BatchStatement();
+        // for (int i = 0; i < 100; i++)
+        // {
+        //     batch.Add(query.Bind(i.ToString(), record));
+        // }
+        //
+        // // var statement = query.Bind("1", Encoding.UTF8.GetBytes("data"));
+        //
+        // var result = session.Execute(batch);
+        //
+        //
+        //
+        // // session.ExecuteAsync(new SimpleStatement(""))
+        //
+        // // User user = mapper.Single<User>("SELECT name, email FROM users WHERE id = ?", userId);
+    }
+}
---
+++ examples/CSharpProd/DB/ScyllaDB/ScyllaInitDBScenario.cs
@@ -0,0 +1,99 @@
+﻿using System.Net;
+using Cassandra;
+using Microsoft.Extensions.Configuration;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Data;
+
+namespace CSharpProd.DB.ScyllaDB;
+
+public class ScyllaDBInitSettings
+{
+    public string ConnectionString { get; set; }
+    public int UserCount { get; set; }
+    public int UserRecordSizeBytes { get; set; }
+    public int InsertBulkSize { get; set; }
+    public string KeySpace { get; set; }
+    public int ReplicationFactor { get; set; }
+    public bool ShouldPopulateDB { get; set; }
+}
+
+public class ScyllaInitDBScenario
+{
+    public ScyllaDBInitSettings DBSettings { get; private set; }
+    public ISession Session { get; private set; }
+    public PreparedStatement InsertQuery { get; private set; }
+    public PreparedStatement GetByIdQuery { get; private set; }
+    public byte[] UserRecord { get; set; }
+
+    public ScenarioProps Create()
+    {
+        return Scenario
+            .Empty("initDB")
+            .WithInit(context =>
+            {
+                DBSettings = context.CustomSettings.Get<ScyllaDBInitSettings>();
+                
+                // var ip = new IPEndPoint(IPAddress.Parse("206.189.62.89"), 9042);
+
+                var hosts = DBSettings.ConnectionString.Split(",");
+                
+                var cluster = Cluster.Builder()
+                    .AddContactPoints(hosts)
+                    .WithCompression(CompressionType.NoCompression)
+                    .Build();
+                
+                Session = cluster.Connect();
+
+                Session.CreateKeyspaceIfNotExists(DBSettings.KeySpace, new Dictionary<string, string>
+                {
+                    { "class", ReplicationStrategies.SimpleStrategy }, { "replication_factor", DBSettings.ReplicationFactor.ToString() }
+                });
+                
+                Session.ChangeKeyspace(DBSettings.KeySpace);
+                
+                //PopulateDB();
+                
+                if (DBSettings.ShouldPopulateDB)
+                    PopulateDB();
+                else
+                    PrepareQueries();
+
+                return Task.CompletedTask;
+            });
+    }
+
+    void PrepareQueries()
+    {
+        InsertQuery = Session.Prepare("INSERT INTO users (id, data) VALUES (?, ?)");
+        GetByIdQuery = Session.Prepare("SELECT id, data FROM users WHERE id=?");
+        UserRecord = Data.GenerateRandomBytes(DBSettings.UserRecordSizeBytes);
+    }
+    
+    void PopulateDB()
+    {
+        Session.Execute(new SimpleStatement(
+            "DROP TABLE IF EXISTS users"
+        ));
+                
+        Session.Execute(new SimpleStatement(
+                // "CREATE TABLE users (id text PRIMARY KEY, data blob) WITH COMPACT STORAGE")
+            "CREATE TABLE users (id text PRIMARY KEY, data blob) WITH COMPACT STORAGE AND compression = {}")
+        );
+
+        PrepareQueries();
+        
+        var userId = 0;
+        while (userId < DBSettings.UserCount)
+        {
+            var batch = new BatchStatement();
+            for (int i = 0; i < DBSettings.InsertBulkSize; i++)
+            {
+                batch.Add(InsertQuery.Bind(userId.ToString(), UserRecord));
+                userId++;
+            }
+
+            Session.Execute(batch);
+        }
+    }
+}
