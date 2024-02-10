052ce6233329238dea06863c0c051ed8a28d58d0
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-10 09:21:50 +0200

    first version

---
+++ examples/Demo/WebBrowserTesting/PlaywrightTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+using NBomber.CSharp;
+
+namespace Demo.WebBrowserTesting;
+
+public class PlaywrightTest
+{
+    public async Task Run()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new  BrowserTypeLaunchOptions {ExecutablePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"});
+
+        var scenario = Scenario.Create("http_scenario", async context =>
+            {
+                var page = await browser.NewPageAsync();
+                await page.GotoAsync("https://nbomber.com");
+
+                return Response.Ok();
+            })
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.KeepConstant(50, TimeSpan.FromSeconds(60))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
---
+++ examples/Demo/WebBrowserTesting/PuppeteerTest.cs
@@ -0,0 +1,30 @@
+﻿using NBomber.CSharp;
+using PuppeteerSharp;
+using Response = NBomber.CSharp.Response;
+
+namespace Demo.WebBrowserTesting;
+
+public class PuppeteerTest
+{
+    public async Task Run()
+    {
+        await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            { Headless = true, ExecutablePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" });
+
+        var scenario = Scenario.Create("http_scenario", async context =>
+            {
+                await using var page = await browser.NewPageAsync();
+                await page.GoToAsync("https://nbomber.com");
+
+                return Response.Ok();
+            })
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.KeepConstant(50, TimeSpan.FromSeconds(60))
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
