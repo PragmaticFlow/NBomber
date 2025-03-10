0c314875de6308c27a7e9a914006a9c455b666a2
User <oleksandr.palamarchuk1310@gmail.com>
2025-03-10 14:51:24 +0200

    Add the gRPC console client

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GrpcGreeterClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
