2aa1e10860679d356b49b4f808d5cea267428a6b
User <oleksandr.palamarchuk1310@gmail.com>
2025-03-10 16:38:25 +0200

    Add protos to the gRPC client project

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace GrpcGreeterClient
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:7117");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
