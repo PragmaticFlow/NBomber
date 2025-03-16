054e8d5ef7af2edcca1dcf9c0ff1cca7f20caca7
antyadev <antyadev@gmail.com>
2025-03-16 23:17:31 +0200

    version 6.0.1

--- examples/Demo/Features/CustomMetrics/CustomMetricsExample.cs
+++ examples/Demo/Features/CustomMetrics/CustomMetricsExample.cs
@@ -3,20 +3,20 @@ using NBomber.CSharp;
 
 namespace Demo.Features.CustomMetrics;
 
-public class CustomMetricExample
+public class CustomMetricsExample
 {
     public void Run()
     {
         // define custom metrics
-        var counter = Metric.CreateCounter("my-counter", "MB");
-        var gauge = Metric.CreateGauge("my-gauge", "KB");
+        var counter = Metric.CreateCounter("my-counter", unitOfMeasure: "MB");
+        var gauge = Metric.CreateGauge("my-gauge", unitOfMeasure: "KB");
 
-        var scenario = Scenario.Create("hello_world_scenario", async context =>
+        var scenario = Scenario.Create("scenario", async context =>
         {
             await Task.Delay(500);
 
-            counter.Add(1);
-            gauge.Set(6.5);
+            counter.Add(1); // tracks a value that may increase or decrease over time
+            gauge.Set(6.5); // set the current value of the metric
 
             return Response.Ok();
         })
@@ -31,10 +31,15 @@ public class CustomMetricExample
         .WithoutWarmUp()
         .WithLoadSimulations(
             Simulation.Inject(rate: 150, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(30)) // keep injecting with rate 150
+        )
+        .WithThresholds(
+            Threshold.Create(metric => metric.Counters.Get("my-counter").Value < 1000),
+            Threshold.Create(metric => metric.Gauges.Get("my-gauge").Value >= 6.5)
         );
 
         var stats = NBomberRunner
             .RegisterScenarios(scenario)
+            //.LoadConfig("./Features/CustomMetrics/nbomber-config.json")
             .Run();
 
         // We can retrieve the final values.
---
+++ examples/Demo/HelloWorld/ScenarioTimerTimeExample.cs
@@ -0,0 +1,29 @@
+﻿using NBomber.CSharp;
+
+namespace Demo.HelloWorld;
+
+public class ScenarioTimerTimeExample
+{
+    public void Run()
+    {
+        var scenario = Scenario.Create("hello_world_scenario", async context =>
+        {
+            var currentScenarioTime = context.GetScenarioTimerTime();
+
+            await Task.Delay(1_000);
+
+            // check log file to see this text
+            context.Logger.Information($"Current scenario time: {currentScenarioTime}");
+
+            return Response.Ok();
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.KeepConstant(copies: 1, during: TimeSpan.FromSeconds(30))
+        );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
