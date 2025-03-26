cd454bee27cab1cd8778c407d04c06d0c38bcfc7
User <oleksandr.palamarchuk1310@gmail.com>
2025-03-26 18:28:02 +0200

    Add logic to controller`s actions

--- examples/WebAppSimulator/Controllers/CookiesAuthenticationController.cs
+++ examples/WebAppSimulator/Controllers/CookiesAuthenticationController.cs
@@ -7,20 +7,38 @@ namespace WebAppSimulator.Controllers
     [ApiController]
     public class CookiesAuthenticationController : ControllerBase
     {
-        private readonly ConcurrentDictionary<string, Guid> GeneratedCookies = new();
+        private readonly ConcurrentBag<Guid> GeneratedCookies = new();
 
         public CookiesAuthenticationController() { }
 
         [HttpPost]
         public Task Login(string login, string password)
         {
+            var authCookie = Guid.NewGuid();
 
+            GeneratedCookies.Add(authCookie);
+            Response.Cookies.Append("auth_hash_cookie", authCookie.ToString());
+
+            return Task.CompletedTask;
         }
 
         [HttpGet]
         public Task<string> GetAccess()
         {
+            var authCookie = Request.Cookies["auth_hash_cookie"];
+            var convertResult = Guid.TryParse(authCookie, out Guid guidAuthCookie);
 
+            if (convertResult)
+            {
+                if (GeneratedCookies.TryPeek(out guidAuthCookie))
+                    return Task.FromResult("Authentication successful.");
+                else
+                    return Task.FromResult("Authentication failed. The auth_hash_cookie is wrong.");
+            }
+            else
+            {
+                return Task.FromResult("The auth_hash_cookie is not valid.");
+            }            
         }
     }
 }
