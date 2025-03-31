0dc5fe8fc948dd8ec8b74469b926ee30f2becade
User <oleksandr.palamarchuk1310@gmail.com>
2025-03-31 17:13:33 +0300

    Add the UserDto class

--- examples/HttpApiSimulator/Contracts/User.cs
+++ examples/HttpApiSimulator/Contracts/User.cs
@@ -7,4 +7,22 @@ namespace WebAppSimulator.Contracts
         public string LastName { get; set; }
         public int Age { get; set; }
     }
+
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public User ToUser()
+        {
+            return new User
+            {
+                Id = 0,
+                FirstName = this.FirstName,
+                LastName = this.LastName,
+                Age = this.Age
+            };
+        }
+    }
 }
