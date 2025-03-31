c8522d4f879013b99cdc2eb3fda0b73ae1e9e57d
User <oleksandr.palamarchuk1310@gmail.com>
2025-03-31 12:21:34 +0300

    Replace the WebAppSimulator with the HttpApiSimulator Web API

--- examples/HttpApiSimulator/Program.cs
+++ examples/HttpApiSimulator/Program.cs
@@ -48,21 +48,8 @@ namespace WebAppSimulator
 
             if (app.Environment.IsDevelopment())
             {
-                app.UseStaticFiles(new StaticFileOptions()
-                {
-                    OnPrepareResponse = context =>
-                    {
-                        context.Context.Response.Headers.Append("Cache-Control", "no-cache, no-store");
-                        context.Context.Response.Headers.Append("Expires", "-1");
-                    }
-                });
-
                 app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                    c.RoutePrefix = string.Empty; 
-                });
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
