60dd829bcbccf43b1c5db25455e7560b01cda715
Anton Moldovan <antyadev@gmail.com>
2026-01-19 19:37:23 +0200

    v6.2.0

--- examples/Demo/Features/RealtimeReporting/TimescaleDB/TimescaleDBReportingExample.cs
+++ examples/Demo/Features/RealtimeReporting/TimescaleDB/TimescaleDBReportingExample.cs
@@ -22,7 +22,12 @@ public class TimescaleDBReportingExample
             var step3 = await Step.Run("buy_product", context, async () =>
             {
                 await Task.Delay(Random.Shared.Next(1000, 2000));
-                return Response.Ok(statusCode: "201", sizeBytes: 30);
+                var value = context.Random.Next(0, 4);
+
+                if (value == 3)
+                    return Response.Fail(sizeBytes: 30, statusCode: "400");
+
+                return Response.Ok(statusCode: "200", sizeBytes: 30);
             });
 
             return Response.Ok(statusCode: "200");
@@ -40,13 +45,13 @@ public class TimescaleDBReportingExample
             var step1 = await Step.Run("login", context, async () =>
             {
                 await Task.Delay(Random.Shared.Next(100, 500));
-                return Response.Ok(statusCode: "200", sizeBytes: 20);
+                return Response.Ok(sizeBytes: 10, statusCode: "200");
             });
 
             var step2 = await Step.Run("get_product", context, async () =>
             {
                 await Task.Delay(Random.Shared.Next(500, 1000));
-                return Response.Ok(statusCode: "200", sizeBytes: 20);
+                return Response.Ok(sizeBytes: 20, statusCode: "200");
             });
 
             var step3 = await Step.Run("buy_product", context, async () =>
@@ -73,9 +78,10 @@ public class TimescaleDBReportingExample
         NBomberRunner
             .RegisterScenarios(scenario, scenario2)
             .LoadInfraConfig("Features/RealtimeReporting/TimescaleDB/infra-config.json")
+            .WithReportingInterval(TimeSpan.FromSeconds(5))
             .WithReportingSinks(new TimescaleDbSink())
-            .WithTestSuite("user_operations")
-            .WithTestName("mongo_db_orders_creation")
+            .WithTestSuite("reporting")
+            .WithTestName("timescale_db_demo")
             .Run();
     }
 }
