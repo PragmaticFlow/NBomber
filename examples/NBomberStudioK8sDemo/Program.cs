604fae766210e4535e57f3f62818371b532cf3ca
antyadev <antyadev@gmail.com>
2026-05-15 11:24:51 +0300

    Added NBomberStudioK8sDemo

---
+++ examples/NBomberStudioK8sDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using NBomber.CSharp;
+using NBomber.Http.CSharp;
+using NBomber.Sinks.Timescale;
+
+var httpClient = Http.CreateDefaultClient();
+
+var scenario = Scenario.Create("my_scenario", async context =>
+{
+    var request = Http.CreateRequest("GET", "https://artifacthub.io/");
+
+    var response = await Http.Send(httpClient, request);
+
+    return response;
+});
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .WithReportingSinks(new TimescaleDbSink())
+    .Run(args);
